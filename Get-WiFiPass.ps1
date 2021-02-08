785e2bd2d070800ae97f88d89d3968ffc5e7d161
ianchan93 <43088744+ianchan93@users.noreply.github.com>
2021-02-09 09:44:10 +1300

    Add files via upload
    This script will help you obtain all saved WiFi passwords on a particular machine.

---
+++ Get-WiFiPass.ps1
@@ -0,0 +1,9 @@
+﻿$profiles = (netsh wlan show profiles) -match '\s{1,}:\s' -replace '.*:\s' , ''
+
+ForEach($i in $profiles){
+
+    $passwords = (netsh wlan show profiles $i key=clear |findstr "Key Content") -replace '.*:\s', ''
+    echo "SSID: $i"
+    echo "Password: $passwords `n"
+    
+}
